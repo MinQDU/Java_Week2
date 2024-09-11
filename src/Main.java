3900b89307cacb041859c896355c064906447d80
MinQ <2024513@daelim.ac.kr>
2024-09-11 17:18:16 +0900

    처음 커밋

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 100;
+        boolean c = true;
+        char d = 65;
+        String e = "easdjk;lsad;lsda;kl";
+
+        System.out.println(a); //안녕 하세요!
+        System.out.println(b); //안녕 하세요!
+        System.out.println(c); //안녕 하세요!
+        System.out.println(d); //안녕 하세요!
+
+    }
+}
